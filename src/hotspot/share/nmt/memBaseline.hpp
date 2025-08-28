3d3484148e4edc184babae4ec5dde695cd2aa1a7
Johan Sjölén <johan.sjolen@oracle.com>
2025-08-28 17:46:26 +0200

    Oh this??

--- src/hotspot/share/nmt/memBaseline.hpp
+++ src/hotspot/share/nmt/memBaseline.hpp
@@ -72,14 +72,14 @@ class MemBaseline {
 
   // Allocation sites information
   // Malloc allocation sites
-  LinkedListImpl<MallocSite>                  _malloc_sites;
+  LinkedListImpl<MallocSite, AnyObj::ARENA>                  _malloc_sites;
 
   // All virtual memory allocations
-  LinkedListImpl<ReservedMemoryRegion>        _virtual_memory_allocations;
+  LinkedListImpl<ReservedMemoryRegion, AnyObj::ARENA>        _virtual_memory_allocations;
 
   // Virtual memory allocations by allocation sites, always in by_address
   // order
-  LinkedListImpl<VirtualMemoryAllocationSite> _virtual_memory_sites;
+  LinkedListImpl<VirtualMemoryAllocationSite, AnyObj::ARENA> _virtual_memory_sites;
 
   SortingOrder         _malloc_sites_order;
   SortingOrder         _virtual_memory_sites_order;
